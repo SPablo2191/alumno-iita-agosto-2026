b3ff83a36157f8860ae485e5836c691396f17dab
SPablo2191 <pablosandoval2191@gmail.com>
2026-08-03 20:15:25 -0300

    registro de alumnos y mostrar uno por uno

---
+++ main.py
@@ -0,0 +1,10 @@
+alumnos = {
+    "43168585" : {
+        "nombre" : "pablo",
+        "apellido": "sandoval",
+        "notas" : [1,2,3]
+    }
+}
+
+for clave in alumnos:
+    print(alumnos[clave])
